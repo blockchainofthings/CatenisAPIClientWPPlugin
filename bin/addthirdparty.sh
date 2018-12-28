995092950a92422c79be4cbd59a7f3842e4db29b
Cláudio Sá de Castro <claudio@hiades.com.br>
2018-12-28 16:39:10 -0200

    Fix script used to move third party code (composer packages) into the plugin's own namespace.

--- bin/addthirdparty.sh
+++ bin/addthirdparty.sh
@@ -28,28 +28,8 @@ for f in $(find $THIRDPARTY_DIR -name "*.php" -print); do
    sed -i "" -E "s/^use +([A-Za-z][A-Za-z0-9_]*\\\)/use Catenis\\\WP\\\\\1/g" $f
 done
 
-for f in $(find $THIRDPARTY_DIR/clue -name "*.php" -print); do
-   sed -i "" -E "s/([^A-Za-z0-9_]\\\\|'| )Clue\\\\/\1Catenis\\\\WP\\\\Clue\\\\/g" $f
-done
-
-for f in $(find $THIRDPARTY_DIR/guzzlehttp -name "*.php" -print); do
-   sed -i "" -E "s/([^A-Za-z0-9_]\\\\|'| )GuzzleHttp\\\\/\1Catenis\\\\WP\\\\GuzzleHttp\\\\/g" $f
-done
-
-for f in $(find $THIRDPARTY_DIR/wyrihaximus -name "*.php" -print); do
-   sed -i "" -E "s/([^A-Za-z0-9_]\\\\|'| )WyriHaximus\\\\/\1Catenis\\\\WP\\\\WyriHaximus\\\\/g" $f
-done
-
-for f in $(find $THIRDPARTY_DIR/react -name "*.php" -print); do
-   sed -i "" -E "s/([^A-Za-z0-9_]\\\\|'| )React\\\\/\1Catenis\\\\WP\\\\React\\\\/g" $f
-done
-
-for f in $(find $THIRDPARTY_DIR/ringcentral -name "*.php" -print); do
-   sed -i "" -E "s/([^A-Za-z0-9_]\\\\|'| )RingCentral\\\\/\1Catenis\\\\WP\\\\RingCentral\\\\/g" $f
-done
-
-for f in $(find $THIRDPARTY_DIR/ratchet -name "*.php" -print); do
-   sed -i "" -E "s/([^A-Za-z0-9_]\\\\|'| )Ratchet\\\\/\1Catenis\\\\WP\\\\Ratchet\\\\/g" $f
+for f in $(egrep -rl --exclude-dir composer --include "*.php" "([^A-Za-z0-9_]\\\\|'|\"| )(Clue|GuzzleHttp|WyriHaximus|React|RingCentral|Ratchet)\\\\" $THIRDPARTY_DIR/*); do
+    sed -i "" -E "s/('|\"| |\t)(\\\\{0,2})(Clue|GuzzleHttp|WyriHaximus|React|RingCentral|Ratchet)\\\\/\1\2Catenis\\\\WP\\\\\3\\\\/g" $f
 done
 
 sed -i "" -E "s/( +')(([A-Za-z][A-Za-z0-9_]*\\\\\\\)+')/\1Catenis\\\\\\\WP\\\\\\\\\2/g" $THIRDPARTY_DIR/composer/autoload_{psr4,static}.php
@@ -59,6 +39,13 @@ sed -i "" -E "s/( +')Catenis\\\\\\\WP\\\\\\\Catenis\\\\\\\WP\\\\\\\'/\1Catenis\\
 sed -i "" -E "s/( +')Evenement'/\1Catenis\\\\\\\WP\\\\\\\Evenement'/g" $THIRDPARTY_DIR/composer/autoload_{namespaces,static}.php
 
 sed -i "" -E "s/('| | \\\\|\(\\\\)Composer\\\\Autoload/\1Catenis\\\\WP\\\\Composer\\\\Autoload/g" $THIRDPARTY_DIR/composer/autoload_real.php
+
+sed -i "" -e ':a' -e 'N' -e '$!ba' -e "s/\(\n\) \{1,\}),\n \{1,\}'[A-Z]' => \n \{1,\}array (\n/\1/g" $THIRDPARTY_DIR/composer/autoload_static.php
+sed -i "" -e "s/^\( \{1,\}'\)[A-Z]\(' => \)$/\1C\2/g" $THIRDPARTY_DIR/composer/autoload_static.php
+sed -i "" -e "s/ => \([0-9]\{1,\}\),$/ => \1+11,/g" $THIRDPARTY_DIR/composer/autoload_static.php
+sed -i "" -e "s/\('Catenis\\\\\\\\WP\\\\\\\\' => 11\)+11/\1/" $THIRDPARTY_DIR/composer/autoload_static.php
+
+mkdir $THIRDPARTY_DIR/evenement/evenement/src/Catenis && mkdir $THIRDPARTY_DIR/evenement/evenement/src/Catenis/WP && mv $THIRDPARTY_DIR/evenement/evenement/src/Evenement $THIRDPARTY_DIR/evenement/evenement/src/Catenis/WP/
 
 # Add missing namespace
 sed -i "" '2i\
